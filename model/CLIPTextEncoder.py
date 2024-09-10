143376f88d9b90451d60e79e3507af0d6be2def7
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-09 21:00:41 -0500

    modified:   model/CLIPTextEncoder.py

--- model/CLIPTextEncoder.py
+++ model/CLIPTextEncoder.py
@@ -1,4 +1,6 @@
 from transformers import AutoTokenizer, CLIPTextModel, CLIPTextModelWithProjection
+import torch.nn as nn
+import torch
 
 
 def CreateTextEncoder(model_name="openai/clip-vit-large-patch16", freeze=False, projection=False):
@@ -18,3 +20,47 @@ def CreateTextEncoder(model_name="openai/clip-vit-large-patch16", freeze=False, 
         for param in TextEncoder.parameters():
             param.requires_grad = False
     return TextEncoder
+
+
+class SSM_input_projection(nn.Module):
+    def __init__(self, config: dict):
+        super(SSM_input_projection, self).__init__()
+        self.input_dim = config["input_dim"]
+        self.num_frames = config["num_frames"]
+        
+        self.textencoder = CreateTextEncoder(
+            model_name=config["textencoder"],
+            freeze=config["freeze"],
+            projection=config["projection"],
+        )
+        
+        self.querries = nn.Conv1d(1, self.num_frames, 1)
+        
+        self.decoder_layer = nn.TransformerDecoderLayer(
+            d_model=self.textencoder.config.hidden_size,
+            nhead=config["nhead"],
+            dim_feedforward=config["dim_feedforward"],
+            dropout=config["dropout"],
+            activation=config["activation"],
+            batch_first=True,
+        )
+        self.projection_block = nn.TransformerDecoder(
+            self.decoder_layer, 
+            num_layers=config["num_layers"]
+        )
+    
+    def forward(self, Token_text: dict):
+        TextEncoderOutput = self.textencoder(**Token_text)
+        last_hidden_state = TextEncoderOutput.last_hidden_state
+        
+        querries = self.querries(last_hidden_state.unsqueeze(1)).squeeze(1)
+        
+        tgt_mask = (1 - torch.tril(torch.ones(self.num_frames, self.num_frames))).bool().to(querries.device)
+        
+        SSM_input = self.projection_block(
+            tgt=querries,
+            memory=last_hidden_state,
+            tgt_mask=tgt_mask
+        )
+        
+        return SSM_input
