d2ffe9aa1d663de463e725f68a31a9b26a0d1c39
diya <diyali@esri.com>
2024-09-11 17:59:29 -0700

    fix tokenizer

--- dataloader/text_tokenizer.py
+++ dataloader/text_tokenizer.py
@@ -2,7 +2,10 @@ from transformers import CLIPTokenizer, CLIPProcessor
 
 class CLIPTextTokenizer:
     def __init__(self, model_dir):
-        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+        if model_dir:
+            self.tokenizer = CLIPTokenizer.from_pretrained(model_dir)
+        else:
+            self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
     
     def tokenize(self, text):
         return self.tokenizer(text, return_tensors="pt", padding=True, truncation=True)
