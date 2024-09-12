9c9a057850d16dd70792cdea64a584942052ae40
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-12 18:59:30 -0500

    modified:   train_eval/train.py

--- train_eval/train.py
+++ train_eval/train.py
@@ -19,7 +19,7 @@ def train_one_epoch(
     
     for batch in metric_logger.log_every(data_loader, print_freq, header):
         videos = batch["mp4"].to(device)
-        captions = tokenizer(batch["txt"], padding=True, return_tensors="pt").to(device)
+        captions = tokenizer.tokenize(batch["txt"]).to(device)
         
         with torch.cuda.amp.autocast(enabled=scaler is not None):
             loss_dict = model(videos, **captions)
