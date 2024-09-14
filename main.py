c2c831855c4d90ce46d22dbad11f5178d268ef8e
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-13 20:59:40 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def parse_args():
     
     # training parameters
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema")  # Choice doesn't affect training
-    parser.add_argument('--seed', default=None, type=int,
+    parser.add_argument('--seed', default=42, type=int,
                         help='seed for initializing training. ')
     parser.add_argument('--device', default='cuda', type=str,
                         help='device to use for training / testing')
