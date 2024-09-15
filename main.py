2f5d58fe724377764c8891855da160b3c998efef
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-15 18:11:00 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -186,7 +186,6 @@ def main(args):
         dataset,
         batch_size=int(args.batch_size // dist.get_world_size()),
         sampler=sampler,
-        num_workers=nw,
         pin_memory=True,
         drop_last=True,
     )
