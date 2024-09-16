d49e0e75ef4f482196df7f2a1032d63b3a65b6b5
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-16 18:24:28 -0500

    modified:   main.py 	modified:   main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def parse_args():
                         help='device to use for training / testing')
     parser.add_argument('--num_workers', default=4, type=int,
                         help='number of data loading workers')
-    parser.add_argument('--batch_size', default=2, type=int,
+    parser.add_argument('--batch_size', default=4, type=int,
                         help='input batch size for training')
     parser.add_argument('--epochs', default=1000, type=int,
                         help='number of epochs to train')
