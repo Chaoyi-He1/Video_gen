632e2369c5186e08aa80a78774e4e2a8463597db
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-17 14:08:26 -0500

    modified:   test.py

--- test.py
+++ test.py
@@ -3,7 +3,27 @@ from transformers import CLIPTextModelWithProjection, CLIPTextModel, AutoTokeniz
 from diffusers.models import AutoencoderKL
 from transformers import CLIPTokenizer
 import os
+from dataloader import create_data_loader, CLIPTextTokenizer
+import yaml
+import torch.utils.data
 
 
 if __name__ == "__main__":
+    config = "config/cfg.yaml"
+    with open(config) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
     
+    dataset, _ = create_data_loader(
+        data_dir=config["data_dir"],
+        video_size=(config["video_height"], config["video_width"]),
+        fps=config["fps"],
+        max_num_frames=config["max_num_frames"],
+    )
+    sampler = torch.utils.data.SequentialSampler(dataset)
+    loader = torch.utils.data.DataLoader(
+        dataset, batch_size=8,
+        sampler=sampler,
+        drop_last=False)
+    
+    for batch in loader:
+        pass
