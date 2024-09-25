bf97be45e76480c5a2d619f4e5b01a6154aed44e
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-25 13:08:57 -0500

    modified:   sample.py

--- sample.py
+++ sample.py
@@ -40,11 +40,11 @@ def parse_args():
                         nargs=argparse.REMAINDER)
     
     # output directory
-    parser.add_argument('--resume', default='trained_models/model_28.pth', type=str, metavar='PATH',
+    parser.add_argument('--resume', default='trained_models/model_48.pth', type=str, metavar='PATH',
                         help='path to latest checkpoint (default none)')
     parser.add_argument('--save_dir', default='sample/', type=str,
                         help='directory to save checkpoints')
-    parser.add_argument('--test-dir', default='sample/', type=str,
+    parser.add_argument('--test-dir', default='data/train_data/labels/', type=str,
                         help='directory to read test results')
     parser.add_argument('--test_num', default=3, type=int,
                         help='number of test results to generate')
