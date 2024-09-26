c8741c2ed14f8a862c09b78d3434740385de34b1
Chaoyi He <chaoyi_he@tamu.edu>
2024-09-25 23:47:36 -0500

    modified:   save_npy.py

--- save_npy.py
+++ save_npy.py
@@ -20,20 +20,19 @@ if __name__ == "__main__":
     dataset, _ = create_data_loader(
         data_dir=config["data_dir"],
         video_size=(config["video_height"], config["video_width"]),
-        fps=config["fps"] * 2,
-        max_num_frames=330,
+        fps=config["fps"],
+        max_num_frames=config["max_num_frames"],
     )
     sampler = torch.utils.data.SequentialSampler(dataset)
     loader = torch.utils.data.DataLoader(
         dataset, batch_size=1,
         sampler=sampler, num_workers=8,
         drop_last=False)
+    os.makedirs(save_dir, exist_ok=True)
     
     for i, batch in tqdm(enumerate(loader)):
-        if i >= 8000:
-            break
         mp4 = batch["mp4"][0].to(torch.uint8).contiguous().numpy()   # mps is a numpy with shape (1, config["max_num_frames"], 3, config["video_height"], config["video_width"])
-        mp4 = mp4[:config["max_num_frames"]*2:2, :, :, :]
+        # mp4 = mp4[:config["max_num_frames"]*2:2, :, :, :]
         video_name = batch["video_name"][0]
         if len(mp4) > 0:
             if not os.path.exists(os.path.join(save_dir, f"{video_name}.npz")) or overwrite:
