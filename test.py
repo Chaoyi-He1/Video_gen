98aa4a9ed06886cde4a7d796df82fae466e65da6
Chaoyi He <chaoyi_he@tamu.edu>
2024-10-23 16:14:12 -0500

    modified:   sample.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,50 +1,137 @@
-import torch
-from transformers import CLIPTextModelWithProjection, CLIPTextModel, AutoTokenizer
-from transformers import CLIPTokenizer
+import argparse
+import datetime
+import glob
+import json
+import math
 import os
-from dataloader import create_data_loader, CLIPTextTokenizer
+import random
+import time
+import pickle
+from pathlib import Path
+import tempfile
+
 import yaml
-import torch.utils.data
-from tqdm import tqdm
+import torch
+import torch.distributed as dist
 import numpy as np
-import os
+from utils import torch_distributed_zero_first
+from tensorboardX import SummaryWriter
+import torch.optim.lr_scheduler as lr_scheduler
+import torch.multiprocessing
+
+import utils.misc as utils
+from dataloader import create_data_loader, CLIPTextTokenizer, PTDataset
+from model import create_model
+from train_eval import *
+from diffusers.models import AutoencoderKL
 import cv2
+import torch.utils.data as data
+from torchvision.utils import save_image
+import matplotlib.pyplot as plt
 
 
-if __name__ == "__main__":
-    config = "config/cfg.yaml"
-    save_dir = "data/train_data/videos_pt_low"
-    overwrite = False
-    with open(config) as f:
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a model')
+    parser.add_argument('--config', default='config/cfg.yaml', type=str,
+                        help='path to config file')
+    parser.add_argument('opts', help='Modify config options using the command-line', default=None,
+                        nargs=argparse.REMAINDER)
+    
+    # output directory
+    parser.add_argument('--resume', default='trained_models/model_2.pth', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default none)')
+    parser.add_argument('--save_dir', default='sample/', type=str,
+                        help='directory to save checkpoints')
+    parser.add_argument('--test-dir', default='data/train_data/', type=str,
+                        help='directory to read test results')
+    parser.add_argument('--test_num', default=3, type=int,
+                        help='number of test results to generate')
+    
+    # training parameters
+    parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema")  # Choice doesn't affect training
+    parser.add_argument('--seed', default=42, type=int,
+                        help='seed for initializing training. ')
+    parser.add_argument('--device', default='cuda', type=str,
+                        help='device to use for training / testing')
+    parser.add_argument('--num_workers', default=4, type=int,
+                        help='number of data loading workers')
+    parser.add_argument('--batch_size', default=20, type=int,
+                        help='input batch size for training')
+
+    parser.add_argument('--print_freq', default=20, type=int,
+                        help='print frequency')
+    
+    # distributed training parameter
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--dist_url', default='env://', type=str,
+                        help='url used to set up distributed training')
+    parser.add_argument('--amp', action='store_false', default=True,
+                        help='use automatic mixed precision training')
+    
+    args = parser.parse_args()
+    assert args.config is not None
+    return args
+
+
+def main(args):
+    # Setup
+    # torch.manual_seed(args.seed)
+    # random.seed(args.seed)
+    # np.random.seed(args.seed)
+    
+    torch.set_grad_enabled(False)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # print the gpu model name to check if it is A100
+    print(torch.cuda.get_device_name())
+    
+    # load hyper parameters
+    with open(args.config) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     
-    dataset, _ = create_data_loader(
-        data_dir=config["data_dir"],
-        video_size=(config["video_height"], config["video_width"]),
-        fps=8,
-        max_num_frames=56,
+    # create model
+    vae = AutoencoderKL.from_pretrained(f"stabilityai/sd-vae-ft-{args.vae}").to(device)
+    
+    # create data loaders
+    dataset = PTDataset(
+        data_dir=config["data_dir"]
     )
-    sampler = torch.utils.data.RandomSampler(dataset)
-    loader = torch.utils.data.DataLoader(
-        dataset, batch_size=1,
-        sampler=sampler, num_workers=8,
-        drop_last=False)
+    sampler = data.RandomSampler(dataset)
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=1,
+        sampler=sampler,
+        pin_memory=True,
+        drop_last=True,
+    )
     
-    for i, batch in tqdm(enumerate(loader)):
-        if i >= 3:
-            break
-        mp4 = batch["mp4"][0]   
-        mp4 = mp4.permute(0, 2, 3, 1).contiguous().to(torch.uint8).numpy()# mps is a numpy with shape (1, config["max_num_frames"], 3, config["video_height"], config["video_width"])
-        # transfer it to video and save it
-        # mp4 = mp4[:112:2, :, :, :]
-        print(mp4.shape)
-        video = [cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) for frame in mp4]
-        video_path = "sample/sample_video_{}.mp4".format(i)
-        # check if the folder exists
-        os.makedirs(os.path.dirname(video_path), exist_ok=True)
-        out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'mp4v'), config["fps"], (config["video_width"], config["video_height"]))
-        for frame in video:
-            out.write(frame)
-        out.release()
-        print("Video saved!")
-    
+    # get one sample from the dataloader
+    for sample in dataloader:
+        videos = sample["mp4"]
+        # take the first video's first frame
+        img = videos[0, 0, ...].to(device)
+        
+        # use the vae to encode the image and then decode it
+        with torch.no_grad():
+            enc_img = vae.encode(img.unsqueeze(0)).latent_dist.sample().mul_(0.18215) 
+            print("Encoded image range: ", enc_img.min(), enc_img.max())
+            dec_img = vae.decode(enc_img / 0.18215).sample.squeeze(0)
+            print("Decoded image range: ", dec_img.min(), dec_img.max())
+        
+        # generate a histogram of the encoded image values
+        plt.hist(dec_img.cpu().numpy().flatten(), bins=100)
+        plt.savefig("histogram.png")
+        # save the original and decoded image
+        img = ((img - img.min()) / (img.max() - img.min()) * 255).permute(1, 2, 0).cpu().numpy().astype(np.uint8)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite("original.jpg", img)
+        
+        dec_img = ((dec_img - dec_img.min()) / (dec_img.max() - dec_img.min()) * 255).permute(1, 2, 0).cpu().numpy().astype(np.uint8)
+        dec_img = cv2.cvtColor(dec_img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite("decoded.jpg", dec_img)
+        
+        break
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
