b8c2d02e26dda5a5b8d5ab9ae61a6f230bc3bda4
Chaoyi-He1 <chaoyi_he@tamu.edu>
2024-10-31 17:03:51 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def parse_args():
                         nargs=argparse.REMAINDER)
     
     # output directory
-    parser.add_argument('--resume', default='trained_models/model_24.pth', type=str, metavar='PATH',
+    parser.add_argument('--resume', default='trained_models/model_43.pth', type=str, metavar='PATH',
                         help='path to latest checkpoint (default none)')
     parser.add_argument('--save_dir', default='trained_models/', type=str,
                         help='directory to save checkpoints')
