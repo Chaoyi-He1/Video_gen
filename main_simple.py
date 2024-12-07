044fe996acce40084806f1971c13d55f02af2872
Chaoyi-He1 <chaoyi_he@tamu.edu>
2024-12-07 17:37:52 -0600

    modified:   main_simple.py

--- main_simple.py
+++ main_simple.py
@@ -43,7 +43,7 @@ def parse_args():
                         nargs=argparse.REMAINDER)
     
     # output directory
-    parser.add_argument('--resume', default='trained_models/simple/model_', type=str, metavar='PATH',
+    parser.add_argument('--resume', default='trained_models/simple/model_26.pth', type=str, metavar='PATH',
                         help='path to latest checkpoint (default none)')
     parser.add_argument('--save_dir', default='trained_models/simple/', type=str,
                         help='directory to save checkpoints')
