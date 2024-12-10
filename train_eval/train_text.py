6b507728274d35a06d3d2566113b82f3ae4b72fd
Chaoyi He <chaoyi_he@tamu.edu>
2024-12-09 22:20:59 -0600

    modified:   main_text.py 	modified:   train_eval/train_text.py

--- train_eval/train_text.py
+++ train_eval/train_text.py
@@ -70,7 +70,7 @@ def train_one_epoch(
             
             metric_logger.update(loss=loss.item())
             
-    metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
     metric_logger.synchronize_between_processes()
     print("Average loss: ", metric_logger.loss.global_avg)
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
