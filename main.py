107d6630611dbe5296e9a73961cd5db1976daeb8
Chaoyi-He1 <chaoyi_he@tamu.edu>
2024-12-10 18:09:32 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ def parse_args():
                         nargs=argparse.REMAINDER)
     
     # output directory
-    parser.add_argument('--resume', default='trained_models/model_124.pth', type=str, metavar='PATH',
+    parser.add_argument('--resume', default='trained_models/model_', type=str, metavar='PATH',
                         help='path to latest checkpoint (default none)')
-    parser.add_argument('--textencoder', default='trained_models/text/model_124.pth', 
+    parser.add_argument('--textencoder', default='trained_models/text/model_22.pth', 
                         type=str, help='path to latest text encoder checkpoint')
     parser.add_argument('--save_dir', default='trained_models/', type=str,
                         help='directory to save checkpoints')
